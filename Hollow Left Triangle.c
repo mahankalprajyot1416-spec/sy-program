03476064b251ea963b1b34833b55507138b5d2c4
mahankalprajyot1416-spec <mahankalprajyot1416@gmail.com>
2025-10-03 10:00:04 +0530

    Create Hollow Left Triangle.c

---
+++ Hollow Left Triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter rows: ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++) {
+        for(int s=1;s<=n-i;s++) printf("  ");
+        for(int j=1;j<=i;j++) {
+            if(j==1 || j==i || i==n) printf("* ");
+            else printf("  ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
