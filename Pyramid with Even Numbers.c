b2f1eb895ebba3bb8fcf226f5e2a6834a2f3c1e1
mahankalprajyot1416-spec <mahankalprajyot1416@gmail.com>
2025-10-03 09:55:18 +0530

    Create Pyramid with Even Numbers.c

---
+++ Pyramid with Even Numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n, even=2;
+    printf("Enter rows: ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++) {
+        for(int s=1;s<=n-i;s++) printf(" ");
+        for(int j=1;j<=i;j++) {
+            printf("%d ", even);
+            even+=2;
+        }
+        printf("\n");
+    }
+    return 0;
+}
