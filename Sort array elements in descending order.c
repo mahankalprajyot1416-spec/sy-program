854a97087ffb14f10bde21d508e6b84fa7cbaf59
mahankalprajyot1416-spec <mahankalprajyot1416@gmail.com>
2025-10-03 09:29:08 +0530

    Create Sort array elements in descending order.c

---
+++ Sort array elements in descending order.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n, arr[100], temp;
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++) scanf("%d", &arr[i]);
+
+    for(int i=0; i<n-1; i++) {
+        for(int j=i+1; j<n; j++) {
+            if(arr[i] < arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    printf("Sorted descending: ");
+    for(int i=0; i<n; i++) printf("%d ", arr[i]);
+    return 0;
+}
