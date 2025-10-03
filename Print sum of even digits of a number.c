b55224db62eccceed0dcdb4a7a1472debeb09f1f
mahankalprajyot1416-spec <mahankalprajyot1416@gmail.com>
2025-10-03 09:13:37 +0530

    Create Print sum of even digits of a number.c

---
+++ Print sum of even digits of a number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n, sum=0;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    for(; n>0; n/=10) {
+        int d = n % 10;
+        if(d % 2 == 0) sum += d;
+    }
+    printf("Sum of even digits = %d\n", sum);
+    return 0;
+}
