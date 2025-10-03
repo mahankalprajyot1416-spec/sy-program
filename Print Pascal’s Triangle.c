9749d0963df0e6b01a79d161cf97250beabe8b2b
mahankalprajyot1416-spec <mahankalprajyot1416@gmail.com>
2025-10-03 09:34:36 +0530

    Create Print Pascal’s Triangle.c

---
+++ Print Pascal’s Triangle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int fact(int n) {
+    int f=1;
+    for(int i=1;i<=n;i++) f*=i;
+    return f;
+}
+int main() {
+    int n;
+    printf("Enter rows: ");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++) {
+        for(int s=0; s<n-i; s++) printf(" ");
+        for(int j=0; j<=i; j++) {
+            printf("%d ", fact(i)/(fact(j)*fact(i-j)));
+        }
+        printf("\n");
+    }
+    return 0;
+}
