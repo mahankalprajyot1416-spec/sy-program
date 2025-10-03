f6604f8cbe9ba4b327d72a9c89e23130e53734e9
mahankalprajyot1416-spec <mahankalprajyot1416@gmail.com>
2025-10-03 10:10:23 +0530

    Create Print sum of first N odd numbers.c

---
+++ Print sum of first N odd numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n, sum=0;
+    printf("Enter N: ");
+    scanf("%d", &n);
+    for(int i=1; i<=2*n; i+=2) {
+        sum += i;
+    }
+    printf("Sum = %d", sum);
+    return 0;
+}
