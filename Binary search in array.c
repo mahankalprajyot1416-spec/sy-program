5af0573f0b43ef21abc8d96189c49f6be5d47cd6
mahankalprajyot1416-spec <mahankalprajyot1416@gmail.com>
2025-10-03 09:29:53 +0530

    Create Binary search in array.c

---
+++ Binary search in array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n, arr[100], key, low, high, mid;
+    printf("Enter size (sorted array): ");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++) scanf("%d", &arr[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    low=0; high=n-1;
+    while(low <= high) {
+        mid = (low + high)/2;
+        if(arr[mid] == key) {
+            printf("Element found at index %d\n", mid);
+            return 0;
+        }
+        else if(arr[mid] < key) low = mid + 1;
+        else high = mid - 1;
+    }
+    printf("Element not found\n");
+    return 0;
+}
