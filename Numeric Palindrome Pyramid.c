7038696f59c1e082eba9beaad24dd338eaf8ac3f
mahankalprajyot1416-spec <mahankalprajyot1416@gmail.com>
2025-10-03 10:01:51 +0530

    Create Numeric Palindrome Pyramid.c

---
+++ Numeric Palindrome Pyramid.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter rows: ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++) {
+        for(int s=1;s<=n-i;s++) printf(" ");
+        for(int j=1;j<=i;j++) printf("%d", j);
+        for(int j=i-1;j>=1;j--) printf("%d", j);
+        printf("\n");
+    }
+    return 0;
+}
