1d470d70ce321578d9709925b6fe83c6884def15
mahankalprajyot1416-spec <mahankalprajyot1416@gmail.com>
2025-10-03 09:47:08 +0530

    Create Rectangle Star Pattern.c

---
+++ Rectangle Star Pattern.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int r, c;
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    for(int i=1; i<=r; i++) {
+        for(int j=1; j<=c; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
