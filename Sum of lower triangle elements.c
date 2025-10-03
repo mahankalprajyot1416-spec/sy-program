13ee7da85a418c1fa81b50ec949ede1c957e317f
mahankalprajyot1416-spec <mahankalprajyot1416@gmail.com>
2025-10-03 09:33:28 +0530

    Create Sum of lower triangle elements.c

---
+++ Sum of lower triangle elements.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n, a[10][10], sum=0;
+    printf("Enter size of square matrix: ");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            scanf("%d", &a[i][j]);
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<=i; j++)
+            sum += a[i][j];
+
+    printf("Sum of lower triangle = %d\n", sum);
+    return 0;
+}
