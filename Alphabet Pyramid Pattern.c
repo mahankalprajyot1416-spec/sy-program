980a8fdc77cc6f4bca0cdaf15e37ae77fcd60e55
mahankalprajyot1416-spec <mahankalprajyot1416@gmail.com>
2025-10-03 09:53:04 +0530

    Create Alphabet Pyramid Pattern.c

---
+++ Alphabet Pyramid Pattern.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter rows: ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++) {
+        char ch='A';
+        for(int s=1;s<=n-i;s++) printf(" ");
+        for(int j=1;j<=i;j++) printf("%c ", ch++);
+        printf("\n");
+    }
+    return 0;
+}
